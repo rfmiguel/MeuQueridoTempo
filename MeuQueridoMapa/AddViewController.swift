600561d05e56dc0cbf631344654c5c5e83879027
Usuário Convidado <Guest@L602MICRO24.local>
2014-10-29 22:11:03 -0200

    webservices

--- MeuQueridoMapa/AddViewController.swift
+++ MeuQueridoMapa/AddViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class AddViewController: UIViewController, UITextFieldDelegate {
 
+    var climaRetorno:Array<Dictionary<String, AnyObject>>? = nil
+    //var locais:Locais =
+    
+    
+    @IBOutlet weak var lbCidade: UILabel!
+    @IBOutlet weak var lbEstado: UILabel!
+    @IBOutlet weak var lbPais: UILabel!
+    
     @IBOutlet weak var txfLocal: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +29,38 @@ class AddViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        let teste:WebService = WebService()
-        teste.getLocal(textField.description)
+        
+        self.buscaCidadeWs(textField.text)
         return true
+    }
+    
+    func buscaCidadeWs(cidade:String)
+    {
+        let webservice:WebService = WebService()
+        climaRetorno = webservice.getLocal(cidade)
+        for climas in self.climaRetorno!
+        {
+            self.lbCidade.text = climas["name"]! as String
+            let sys = climas["sys"]! as Dictionary<String,AnyObject>
+            self.lbPais.text = sys["country"]! as String
+            
+//            locais.cidade = climas["name"]! as String
+//            locais.lat =
+//            locais.long =
+//            @NSManaged var imagemClima: String
+//            @NSManaged var descricaoClima: String
+//            @NSManaged var temperatuma: NSDecimalNumber
+//            @NSManaged var tempMaxima: NSDecimalNumber
+//            @NSManaged var tempMinina: NSDecimalNumber
+//            @NSManaged var humidade: NSNumber
+
+            
+        }
+        
+        
+        
+        txfLocal.resignFirstResponder()
+        
     }
 
 }
--- MeuQueridoMapa/WebService.swift
+++ MeuQueridoMapa/WebService.swift
@@ -10,18 +10,45 @@ import UIKit
 
 class WebService: NSObject {
     
+    var clima:Array<Dictionary<String, AnyObject>>? = nil
+    var _pathWsGoogle:String = "http://maps.googleapis.com/maps/api/geocode/json?latlng="
     
-    func getLocal(local:String){
     
-        let url:NSURL = NSURL(string: "http://api.openweathermap.org/data/2.5/find?q=".stringByAppendingString(local).stringByAppendingString("&units=metric&LANG=pt") )
-    
+    func getLocal(local:String) -> Array<Dictionary<String, AnyObject>>
+    {
+        
+        println()
+        
+//        let localformat = local.stringByAddingPercentEscapesUsingEncoding(encoding:)
+        let newLocal:String = local.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        let strUrl:String = "http://api.openweathermap.org/data/2.5/find?q=".stringByAppendingString(newLocal).stringByAppendingString("&units=metric&LANG=pt")
+        let url:NSURL = NSURL(string: strUrl)
+        println(strUrl)
+
         let request:NSURLRequest = NSURLRequest(URL: url )
 
+        
         var error = NSErrorPointer()
         var response = AutoreleasingUnsafeMutablePointer<NSURLResponse?>()
         var data:NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse: response, error: error)
+        
         var json = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.allZeros, error: error) as Dictionary<String, AnyObject>
-        var datastring:String = NSString(data: data!, encoding: NSUTF8StringEncoding)
-        println(datastring)
+        self.clima = json["list"]! as? Array<Dictionary<String, AnyObject>>
+        
+        return self.clima!
+        
     }
+    
+    func getState(lat:String,long:String) -> String
+    {
+        
+        let url:NSURL = NSURL(string: _pathWsGoogle.stringByAppendingString(lat).stringByAppendingString(",").stringByAppendingString(long).stringByAppendingString("&sensor=true_or_false") )
+        let request:NSURLRequest = NSURLRequest(URL: url )
+        
+        
+        
+        return ""
+    }
+    
 }
